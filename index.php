e80d55d32e491d982027ab1c8b3d7f272380349b
rednaxela700 <ohleg@windowslive.com>
2020-04-26 22:04:11 +0200

    created a User class, used a static func to retrieve a private field

--- index.php
+++ index.php
@@ -1,42 +1,13 @@
 <?php
 
-abstract class Customer
-// abstract can be a base class, as it cannot be instantiated, but we can create subclasses from it
-{
-    private $id = 1; // prop can be accessed outside the class
-    private $name; // can be accessed only inside the class
-    protected $email; //can't be accessed outside, but can be accessed inside another class
-    private $balance;
+class User{
+    public $username;
+    public $password;
+    private static $passwordLength = 5; //static is used as value that won't be changed
 
-    public function __construct($id, $name, $email, $balance)
-    {
-        $this->id = $id;
-        $this->name = $name;
-        $this->email = $email;
-        $this->balance = $balance;
-//        echo 'The Constructor Run';
+    public static function getPasswordLength() {
+        return self::$passwordLength;
     }
-
-    public  function getEmail() {
-        return $this->email;
-    }
-
 }
 
-class  Subscriber extends Customer{
-    public $plan;
-
-    public function __construct($id, $name, $email, $balance, $plan)
-    {
-        parent::__construct($id, $name, $email, $balance);
-            $this->plan = $plan;
-    }
-
-}
-
-//instantiate a class
-//$customer = new Customer(1,'John Doe', 'hello@as.de', 1300);
-$subscriber = new Subscriber(2,'Jane Doe', 'hello@fem.ai', 5000, 'user');
-
-//echo $customer->getEmail();
-echo $subscriber->getEmail();
+echo User::getPasswordLength();
