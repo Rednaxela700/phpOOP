148006206f807666b614df538c41a268d95ab297
rednaxela700 <ohleg@windowslive.com>
2020-04-26 21:49:52 +0200

    created Subscriber class as Customer extension, referred to parent constructor and echoed result

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ class Customer
 {
     private $id = 1; // prop can be accessed outside the class
     private $name; // can be accessed only inside the class
-    private $email; //can't be accessed outside, but can be accessed inside another class
+    protected $email; //can't be accessed outside, but can be accessed inside another class
     private $balance;
 
     public function __construct($id, $name, $email, $balance)
@@ -22,7 +22,20 @@ class Customer
 
 }
 
+class  Subscriber extends Customer{
+    public $plan;
+
+    public function __construct($id, $name, $email, $balance, $plan)
+    {
+        parent::__construct($id, $name, $email, $balance);
+            $this->plan = $plan;
+    }
+
+}
+
 //instantiate a class
 $customer = new Customer(1,'John Doe', 'hello@as.de', 1300);
+$subscriber = new Subscriber(2,'Jane Doe', 'hello@fem.ai', 5000, 'user');
 
-echo $customer->getEmail();
+//echo $customer->getEmail();
+echo $subscriber->getEmail();
