5368f6b8a5e2006381eda0d0f6691465213a0507
rednaxela700 <ohleg@windowslive.com>
2020-04-26 21:28:11 +0200

    created a new instance of Customer class, used public function

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
 class Customer{
-    public $id; // prop can be accessed outside the class
-    private $name; // can be accessed only inside the class
-    protected $email; //can't be accessed outside, but can be accessed inside another class
-    public $location;
+    public $id = 1; // prop can be accessed outside the class
+    public $name; // can be accessed only inside the class
+    public $email; //can't be accessed outside, but can be accessed inside another class
+    public $balance;
+
+     public function getCustomer($id) {
+         $this->id = $id;
+         return 'John Doe';
+     }
 }
+//instantiate a class
+$customer = new Customer;
+
+echo $customer->getCustomer(1);
