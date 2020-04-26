251b544270a4f371efe5f29b3bb13c7bcdf11b83
rednaxela700 <ohleg@windowslive.com>
2020-04-26 21:36:13 +0200

    added constructor fields, echoed field from obj instance

--- index.php
+++ index.php
@@ -1,26 +1,24 @@
 <?php
-class Customer{
+
+class Customer
+{
     public $id = 1; // prop can be accessed outside the class
     public $name; // can be accessed only inside the class
     public $email; //can't be accessed outside, but can be accessed inside another class
     public $balance;
 
-    public function __construct()
+    public function __construct($id, $name, $email, $balance)
     {
-        echo 'The Constructor Run';
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->balance = $balance;
+//        echo 'The Constructor Run';
     }
 
-    public function getCustomer($id) {
-         $this->id = $id;
-         return 'John Doe';
-     }
-
-     public function __destruct()
-     {
-         echo 'The Destructor Run';
-     }
 }
-//instantiate a class
-$customer = new Customer;
 
-echo $customer->getCustomer(1);
+//instantiate a class
+$customer = new Customer(1,'John Doe', 'hello@as.de', 1300);
+
+echo $customer->name;
